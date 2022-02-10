45276289f30d7e6e118deb1c7b3b5c5be4ab0a93
Rakib0101 <rakib.01jan@gmail.com>
2022-02-10 12:59:48 +0600

    update: slider added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,17 @@ function mobileMenu() {
         mobileMenu.style.display = "block";
     }
 }
+
+//Swiper
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
